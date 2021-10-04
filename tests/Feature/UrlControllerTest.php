3191a31034a88fa99333092fec0a046358965096
yulia633 <ignat633yulia@yandex.ru>
2021-10-04 11:54:45 +0300

    add assert for the test store

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -85,6 +85,7 @@ class UrlControllerTest extends TestCase
 
         $data = ['url' => 'https://google.com'];
         $response = $this->post(route('urls.store'), $data);
+        $response->assertSessionHasErrors(['name']);
 
         $response->assertRedirect();
 
